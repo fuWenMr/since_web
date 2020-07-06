c3cc3d6c8801a3022ffa12ea3f85729edf670d42
fuWenMr <nyfjr@icloud.com>
2020-07-06 15:28:29 +0800

    打包文件独立css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const outputPath = path.resolve(__dirname, 'build');
 
 module.exports = {
@@ -13,20 +14,23 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }),
       },
       {
         test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader',
-        ],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader'],
+        }),
       },
     ],
   },
+
+  plugins: [
+    new ExtractTextPlugin('styles.css'),  //打包后的文件名
+  ],
 
 };
