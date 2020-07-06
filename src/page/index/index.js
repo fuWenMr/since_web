93bb1c7081962d0dce046b392cccf2e2b34ee499
fuWenMr <nyfjr@icloud.com>
2020-07-06 20:55:31 +0800

    演示按需加载

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,12 @@
 import './index.less';
 import umbrella from 'umbrella-storage';
 import { debug1 } from '../../util/debug';
+// import { random } from 'lodash';
+import random from 'lodash/random';
 debug1('hello');
 umbrella.setLocalStorage('app', { appId: '123' });
+
+console.log('随机数', random(1,5));
+
 console.log('这是页面index heelo world');
 console.log('从stroage中取出的内容', umbrella.getLocalStorage('app'))
