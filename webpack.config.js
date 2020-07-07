0dd78bf90e03ce2936be0474e1c934af9af9df46
luoxinyuan <1019558287@qq.com>
2020-07-07 10:59:45 +0800

    entry和htmlWebpackPlugin自动生成的实现

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,44 @@ const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const outputPath = path.resolve(__dirname, 'build');
 
+function genHtmlWebpackPlugin(chunksnames){
+  let ret = [];
+  for (let i in chunksnames) {
+    let chunksname = chunksnames[i];
+    ret[i] = new HtmlWebpackPlugin({
+      title: `森思教学的${chunksname}页面`,
+      filename: `page/${chunksname}/index.html`,
+      template: `./src/page/${chunksname}/index.html`,
+      minify: false,
+      chunks: [`${chunksname}`],
+    });
+  }
+  return ret;
+}
+
+function genEntry(chunksnames){
+  let ret = {};
+  for (let chunksname of chunksnames) {
+    ret[chunksname] = `./src/page/${chunksname}/index.js`;
+  }
+  return ret;
+}
+
+//新增页面只需在此添加你的模块就好
+let pages = ['index','login','user'];
+entrys = genEntry(pages);
+console.log(entrys)
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+htmlWebpackPlugin = genHtmlWebpackPlugin(pages);
+// console.log(htmlWebpackPlugin)
+
+
 module.exports = {
-  entry: {
-    index: './src/page/index/index.js',
-    login: './src/page/login/index.js',
-  },
+  entry:genEntry(pages),
   output: {
     filename: 'page/[name]/index.b.js',
     path: outputPath,
   },
-
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       use: ['style-loader', 'css-loader']
-  //     },
-  //     {
-  //       test: /\.less$/,
-  //       use: ['style-loader','css-loader', 'less-loader'],
-  //     },
-  //   ],
-  // },
 
   module: {
     rules: [
@@ -52,20 +68,7 @@ module.exports = {
       allChunks: true,
       filename: 'page/[name]/[name].css',
     }),  //打包后的文件名
-    new HtmlWebpackPlugin({
-      title: '森思教学的indx页面',
-      filename: 'page/index/index.html',
-      template: './src/page/index/index.html',
-      minify: false,
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      title: '森思教学的login页面',
-      filename: 'page/login/index.html',
-      template: './src/page/login/index.html',
-      minify: false,
-      chunks: ['login'],
-    }),
+    ...htmlWebpackPlugin,
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
